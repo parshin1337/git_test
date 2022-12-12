0d0ddd3736aebb0e929b3c8a9f2187b2c9a64ac2
parshin1337 <chipara@mail.ru>
2022-12-12 12:20:46 +0300

    Создали метод area для Circle

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from math import pi
 class Circle:
     def __init__(self, r):
         self._r = r
 
+    def area(self):
+        return pi * (self._r ** 2)
+
 
 main_circle = Circle(2)
+print(main_circle.area())
