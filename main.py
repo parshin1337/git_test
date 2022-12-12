db4c36e14e05c2a75aadb458acf41fa28b5ed7c2
parshin1337 <chipara@mail.ru>
2022-12-12 12:18:45 +0300

    Создали новый Класс

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-class Rectangle:
-    def __init__(self, a, b):
-        self._a = a
-        self._b = b
-
-    def area(self):
-        return self._a * self._b
-
-    def get_height(self):
-        return self._a
-
-    def get_width(self):
-        return self._b
+class Circle:
+    def __init__(self, r):
+        self._r = r
 
 
-main_rect = Rectangle(5, 4)
-print(main_rect.area())
+main_circle = Circle(2)
